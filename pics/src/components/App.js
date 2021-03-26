6aa8a8a1bc75ca3d610e0dd3454ede7b8467545d
Michael Troia <troia.michael.a@gmail.com>
2021-03-26 12:18:20 -0400

    Unsplash connected

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -1,12 +1,24 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar'
 
-const App = () => {
-    return (
-        <div className="ui container" style={{ marginTop: '10px'}}>
-            <SearchBar />
-        </div>
-    );
-};
+class App extends React.Component {
+    onSearchSubmit(term) {
+        axios.get('https://api.unsplash.com/search/photos', {
+            params: { query: term },
+            headers: {
+                Authorization: "Client-ID fNwRU3hW68ypPnnnFy3RzgmVUem65LBuFQXmNkIju9w"
+            }
+        });
+    };
+
+    render() {
+        return (
+            <div className="ui container" style={{ marginTop: '10px'}}>
+                <SearchBar onSubmit={this.onSearchSubmit} />
+            </div>
+        );
+    }
+}
 
 export default App;
