eb7ec39fa7054ea5eb240a20cd433637b8f528b1
Michael Troia <troia.michael.a@gmail.com>
2021-03-30 12:01:45 -0400

    feat: set div height to adjust to image height from ref

--- pics/src/components/ImageCard.js
+++ pics/src/components/ImageCard.js
@@ -4,6 +4,7 @@ class ImageCard extends React.Component {
     constructor(props) {
         super(props);
 
+        this.state = { spans: 0 }
         this.imageRef = React.createRef();
     }
 
@@ -12,13 +13,16 @@ class ImageCard extends React.Component {
     }
 
     setSpans = () => {
-        console.log(this.imageRef.current.clientHeight);
+        const height = this.imageRef.current.clientHeight;
+        const spans = Math.ceil(height / 10);
+
+        this.setState({ spans })
     }
 
     render() {
         const { description, urls } = this.props.image;
         return (
-            <div>
+            <div style={{ gridRowEnd: `span ${this.state.spans}` }}>
                 <img 
                 ref={this.imageRef}
                 alt={description}
