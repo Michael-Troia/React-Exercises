488f46400665f28992c0a4af90c03c9865e90b6a
Michael Troia <troia.michael.a@gmail.com>
2021-03-30 10:45:43 -0400

    CSS grid and ImageCard Component setup

--- pics/src/components/ImageList.js
+++ pics/src/components/ImageList.js
@@ -1,10 +1,12 @@
+import './ImageList.css';
 import React from 'react';
+import ImageCard from './ImageCard';
 
 const ImageList = (props) => {
-    const images = props.images.map(({ description, id, urls} ) => { //destructured image properties
-        return <img key={id} src={urls.regular} alt={description} />
+    const images = props.images.map((image) => { //destructured image properties
+        return <ImageCard key={image.id} image={image} />;
     });
-    return <div>{images}</div>
+    return <div className="image-list">{images}</div>;
 };
 
 export default ImageList;
