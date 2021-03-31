9a9a1f82f1d647105093507dd045ee9b08f3980d
Michael-Troia <troia.michael.a@gmail.com>
2021-03-31 12:39:56 -0400

    search bar input

--- youtube/src/components/App.js
+++ youtube/src/components/App.js
@@ -4,7 +4,9 @@ import SearchBar from './SearchBar';
 class App extends React.Component {
     render() {
         return (
-            <div><SearchBar /></div>
+            <div className="ui container"> 
+                <SearchBar />
+            </div>
         ); 
     }
 }
--- youtube/src/components/SearchBar.js
+++ youtube/src/components/SearchBar.js
@@ -1,8 +1,31 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+    state = { term: '' };
+
+    onInputChange = event => {
+        this.setState({ term: event.target.value });
+    };
+
+    onFormSubmit = event => {
+        event.preventDefault();
+    }
+
     render() {
-        return <div>SearchBar</div>
+        return (
+            <div className="search-bar ui segment">
+                <form onSubmit={ this.onFormSubmit } className="ui form">
+                    <div className="field">
+                        <label>Video search</label>
+                        <input 
+                            type="text"
+                            value={ this.state.term }
+                            onChange={ this.onInputChange }
+                        />
+                    </div>
+                </form>
+            </div>
+        );
     }
 }
 
